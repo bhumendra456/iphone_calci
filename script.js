21448468db25e480f6821b4f20b59cfb4b2f064d
Bhumendra Pardhi <156424210+bhumendra456@users.noreply.github.com>
2024-10-04 19:52:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let expression=document.querySelector('.expression');
+let answer=document.querySelector('.answer');
+let ac=document.querySelector('.ac');
+
+ac.addEventListener("click",()=>{
+    expression.innerHTML= "";
+     answer.innerHTML= "";
+
+});
